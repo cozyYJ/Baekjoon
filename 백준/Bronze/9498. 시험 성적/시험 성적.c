b705688d9b07f8a1288d0dd5836b84bbb482db2a
강연재 Yeonjae Kang <hikyj1701@gmail.com>
2022-10-09 04:05:08 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+    scanf("%d", &score);
+    char grade;
+        if(score>=90 && score<=100)
+            grade='A';
+        else if(score>=80 && score<=89)
+            grade='B';
+        else if(score>=70 && score<=79)
+            grade='C';
+        else if(score>=60 && score<=69)
+            grade='D';
+        else
+            grade='F';
+    printf("%c", grade);
+    
+    return 0;
+}
