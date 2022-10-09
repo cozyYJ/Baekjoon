abfc8993fe5e3f4471aa2539375ad34fe9b37c20
강연재 Yeonjae Kang <hikyj1701@gmail.com>
2022-10-09 16:14:10 +0900

    [Bronze III] Title: 오븐 시계, Time: 236 ms, Memory: 18428 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+
+        int min = 60*a+b;
+        min += c;
+
+        int h = (min/60) % 24;
+        int m = min%60;
+
+        System.out.print(h+" "+m);
+    }
+}
